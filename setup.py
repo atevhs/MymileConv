0c5e2f88c73c1a4f887b49ea8d86b3bf0960a52d
Shveta Sharma <62797751+atevhs@users.noreply.github.com>
2022-12-22 12:06:58 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A test python package',
-	url='https://github.com/khalad-hasan/myMileConverter',
+	url='https://github.com/atevhs/MymileConv',
     author='Shv',
     author_email='atevhs@gmail.com'
 )
